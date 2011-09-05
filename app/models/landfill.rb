4e68de018400d27be476f5b7c34c423181a759a3
kuahyeow <kuahyeow@gmail.com>
2011-09-05 21:26:56 +1200

    Landfill model

---
+++ app/models/landfill.rb
@@ -0,0 +1,2 @@
+class Landfill < ActiveRecord::Base
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110901091115) do
+ActiveRecord::Schema.define(:version => 20110903032320) do
 
   create_table "councils", :force => true do |t|
     t.string   "name"
     t.text     "website"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "landfills", :force => true do |t|
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
