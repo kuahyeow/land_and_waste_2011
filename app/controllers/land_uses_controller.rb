43fabeb293147109d08b266fbeea699f9a246abd
kuahyeow <kuahyeow@gmail.com>
2011-09-14 20:34:25 +1200

    Restrict via dropdown

--- app/controllers/land_uses_controller.rb
+++ app/controllers/land_uses_controller.rb
@@ -3,7 +3,5 @@ class LandUsesController < ApplicationController
     @lands = Land.order('region_id, land_type, year').all(:include => :region).group_by{|l| [l.region_id, l.land_type]}.map do |key, pair|
       pair.size == 2 ? pair : [nil, pair].flatten
     end
-
-    # raise @lands.first.inspect
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,30 @@
 module ApplicationHelper
+  def land_use_class(land)
+    case land.land_type
+    when 'Cropland - Annual'
+      'croplands'
+    when 'Cropland - Perennial'
+      'croplands'
+    when 'Grassland - High producing'
+      'grasslands'
+    when 'Grassland - Low producing'
+      'grasslands'
+    when 'Grassland - With woody biomass'
+      'grasslands'
+    when 'Natural Forest'
+      'forests'
+    when 'Other'
+      'other'
+    when 'Planted Forest - Pre-1990'
+      'forests'
+    when 'Post 1989 Forest'
+      'forests'
+    when 'Settlements'
+      'settlements'
+    when 'Wetland - Open water'
+      'wetlands'
+    when 'Wetland - Vegetated non forest'
+      'wetlands'
+    end
+  end
 end
