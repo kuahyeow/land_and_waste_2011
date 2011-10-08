55b28d6068b792f0b028acfe47b1eaa9840604af
kuahyeow <kuahyeow@gmail.com>
2011-10-08 20:44:42 +1300

    More moving away from mapbox tilestream pro, and start using wax, modestmaps natively

---
+++ public/javascripts/deadsea.js
@@ -0,0 +1,42 @@
+/* licensed under BSD - https://github.com/tmcw/deadsea */
+(function(context) {
+    var deadsea = {};
+
+    function isFamily(parent, child) {
+        if (parent === child) return true;
+         var node = child.parentNode;
+         while (node !== null) {
+             if (node === parent) {
+                 return true;
+             } else {
+                 node = node.parentNode;
+             }
+         }
+         return false;
+    }
+
+    deadsea.blockScrollInto = function(el) {
+        var locked = true;
+        // This is not compatible with IE.
+        if (!window.addEventListener) return this;
+
+        function handleMouseWheel(e) {
+            if (!isFamily(el, e.target)) {
+                locked = true;
+            } else if (locked) {
+                e.stopPropagation();
+            }
+        }
+
+        window.addEventListener('mousewheel', handleMouseWheel, true);
+        window.addEventListener('DOMMouseScroll', handleMouseWheel, true);
+
+        el.addEventListener('mousemove', function mousemove(e) {
+            locked = false;
+        }, false);
+
+        return this;
+    };
+
+    this.deadsea = deadsea;
+})(this);
